1dc30ef21de7c19e5b79dfef15ac2721c78aeb06
SebastianLedesma <ricardoledesmainfolaboral@gmail.com>
2021-09-09 16:48:06 -0300

    Arreglo error en enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCR-feXnS-DfKT7tLFx6h-ovuZh6Scay3Y",
+    authDomain: "saladejuegos-b3881.firebaseapp.com",
+    projectId: "saladejuegos-b3881",
+    storageBucket: "saladejuegos-b3881.appspot.com",
+    messagingSenderId: "285008189542",
+    appId: "1:285008189542:web:42a1f9ba89862c33e1b5a7"
+  }
 };
